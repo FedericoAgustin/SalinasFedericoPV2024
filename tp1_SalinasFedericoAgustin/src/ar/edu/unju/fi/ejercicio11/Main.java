83e28f52bb2f901fa94f633e52e53aec4d15236b
Federico <Federico@DESKTOP-8PSI818>
2024-04-12 23:37:47 -0300

    Se sube la resolución del ejercicio 11 requerido para la asistencia de la comision 3 de práctica (profesor Juan Carlos Rodriguez).

---
+++ tp1_SalinasFedericoAgustin/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int j = 40;
+		for (int i = 2; i <= 41; i++) {
+			System.out.print(j * i + " ");
+			--j;
+		}
+
+	}
+
+}
