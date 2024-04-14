c0a5441e6d744374a507d1667439c144ff4f0b59
Federico <Federico@DESKTOP-8PSI818>
2024-04-13 21:22:10 -0300

    Se sube resolución del ejercicio2.

---
+++ tp1_SalinasFedericoAgustin/src/ar/edu/unju/fi/ejercicio2/Ejercicio2.java
@@ -0,0 +1,30 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+
+		String paisNombre;
+		int edadPersona;
+		float alturaEdificio;
+		double precioProducto;
+		String numeroTelefono;
+		double coseno;
+
+		paisNombre = "Perú";
+		edadPersona = 22;
+		alturaEdificio = (float) 10.5;
+		precioProducto = 20.99;
+		numeroTelefono = "3885239311";
+		coseno = Math.cos(0.5);
+
+		System.out.println("Nombre del País: " + paisNombre);
+		System.out.println("Edad de la persona: " + edadPersona);
+		System.out.println("Altura del edificio: " + alturaEdificio);
+		System.out.println("Precio del producto: " + precioProducto);
+		System.out.println("Número de teléfono: " + numeroTelefono);
+		System.out.println("Coseno: " + coseno);
+
+	}
+
+}
